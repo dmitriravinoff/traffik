857e066a0f170976448a973dff27ea6d46db4e6d
Leif Glynn <LeifGlynn@gmail.com>
2012-11-26 20:42:32 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traffik::Application.config.secret_token = '33186e76f3333f570f743b3adc8623cdaa9cf56b35c96746abf4a7ce75ef4870d8e84032b2e3d596d15297d576916cfeff884bc686b6097c7232a26d7b9aaee6'
